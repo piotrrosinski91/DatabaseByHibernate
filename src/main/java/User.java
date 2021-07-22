39a9d2990d5ae1bcb79e3077c94fec3bcc8d2aa1
Piotr Rosiński <pr.rosinski@gmail.com>
2021-07-22 19:58:30 +0200

    new branch and application properties

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,28 @@
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private int age;
+    private String city;
+
+    public User(String username, int age, String city) {
+        this.username = username;
+        this.age = age;
+        this.city = city;
+    }
 }
